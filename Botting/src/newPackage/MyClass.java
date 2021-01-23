98cc1b6feca2a04be4b12e8ff086956369ac62b9
alexcook <alexcook@alexs-mbp.home>
2021-01-23 18:44:55 -0500

    Java Selenium Automation Test 2

--- Botting/src/newPackage/MyClass.java
+++ Botting/src/newPackage/MyClass.java
@@ -23,11 +23,13 @@ public class MyClass {
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
+		//loads url
 		driver.get("https://www.google.com/");
 
+		//enters search query in google search bar
 		driver.findElement(By.name("q")).sendKeys("BrowserStack Guide");
 		
-//		// locator for Google search button
+		//finds and clicks search button
 		WebElement searchIcon = driver.findElement(By.name("btnK"));
 		searchIcon.click();
 		
